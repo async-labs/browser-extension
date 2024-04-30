79f98fefb371b5d7ff62eeb31d2f97d52851526a
BatAmar Battulga <mrbatamar@gmail.com>
2024-04-30 18:28:03 +0800

    removed test code

--- chapters/5-end/server/src/textract.ts
+++ chapters/5-end/server/src/textract.ts
@@ -18,22 +18,8 @@ export const parsePdf = async (url: string): Promise<string> => {
   return getText(blocks);
 };
 
-function waitFor16Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('ok');
-    }, 1000); // 1 second in milliseconds
-  });
-}
-
 export const extractPdf = async (url: string) => {
-  // const content = await parsePdf(url);
-
-  await waitFor16Seconds();
-
-  const content = `
-  Gabrielle-Anne Salud\nSan Diego, California II (619) 278-9013 II gabbysalud@gmail.com\nEXPERIENCE\nPetDx, San Diego - Clinical Laboratory Technician /\nMARCH 2022 - APRIL 2023\nDetermines the acceptability of specimens for testing according to established criteria\nManually extracts cell-free and genomic DNA from fractionated blood and tissue\nsamples while ensuring viability and integrity throughout the test process\nAnalyzes extracted sample quality through automated gel electrophoresis and\nfluorometry\nFragments and constructs Illumina NGS sequencing libraries from extracted cell-free\nand genomic DNA\nPerforms library multiplexing and Whole Genome Sequencing on the Illumina NovaSeq\n6000 Sequencing System\nMonitors and assesses laboratory cleaning efficiency by swipe testing laboratory and\nequipment surfaces monthly via qPCR\nAssists with other essential laboratory activities, including reagent qualification,\ninventory management, procedure writing, preventive maintenance, inspection\npreparation, assay validation, and troubleshooting investigations\nBiocept, San Diego - Clinical Laboratory Assistant /\nJULY 2020 - MARCH 2022\nAccessioned and processed incoming laboratory specimen in a high volume setting\nContributed to the validation and phase-in of COVID-19/Flu clinical assay\nManually extracted RNA from human plasma and cerebrospinal fluid for cancer\nmutation screening\nPerformed automated TNA isolation on QiaSymphony instrument for cancer mutation\nscreening\nResponsible for reconciliation and troubleshooting of samples that did not adhere to\nrequired criteria\nAdhered to BSL2 requirements and regulations\nCleaned company-wide COVID-19 database for streamlined use and functionality\nCreated laboratory technician tasklist for all shifts for efficient workflow\nCoal Point Oil Reserve, Santa Barbara - Laboratory Researcher Intern\nSEPTEMBER 2019 - JUNE 2020\nAcquired water samples from various bodies of water\nAnalyzed water samples for varying debris and organisms\nIdentified and quantified different water invertebrates\nQuantified and interpreted data to report trends and similarities\nSterilized and arranged lab equipment for proper use\nSUHi Foundation, San Diego - Data Entry Specialist\nJUNE 2018-SEPTEMBER 2018\nReconnected with alumni who received scholarships to gather data\nCompiled information database to track college and post-collegiate activities\nComposed articles that discusses scholar spotlights and organization specialties\nCategorized data to monitor any trends within specific scholarships\nEDUCATION\nUniversity of California - Santa Barbara\nEnvironmental Sciences\nSEPTEMBER 2016 - JUNE 2020\nLABORATORY SKILLS\nInstrument Familiarity: Centrifuges, microfuges, vacufuges, vortexers, mixers, ovens, sonicators,\nthermocyclers, vacuum manifolds\nHamilton Microlab Star\nQuantStudio 5 and 7 Flex\nKingFisher Flex\nTecan Plate Reader Infinite Pro\nQIASymphony\nEPmotion\nep96XL\nAgilent TapeStation\nIllumina NovaSeq 6000 Sequencing System\nlon Chef Systems and lon Torrent Semiconductor\nSequencer\nAutomatic and Manual Pipetting\nReagent and Buffer Preparation and\nHazardous Waste/Substance Disposal\nQualification\nBiosafety Cabinet/Fume Hood Use\nPaperwork Upkeep and Good\nInstrument Maintenance, Calibration,\nDocumentation Practice\nand Validation\nADDITIONAL SKILLS\nTagalog and English\nSoft skills: Communication, independence, team-oriented, highly organized, exceedingly efficient\nHard skills: MS Office, Google Workspace, articulate record-keeping, quality control, LIMS use and\nnavigation
-  `;
+  const content = await parsePdf(url);
 
   return {
     wholeContent: content,
